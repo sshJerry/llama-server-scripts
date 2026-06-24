9bbbee33fef0de9f91e620602355395cad963602
Jerry <defineantoine@gmail.com>
2026-06-24 02:23:13 -0400

    fix formatting

--- run-Qwen3.6-35B-A3B-MTP-heretic.sh
+++ run-Qwen3.6-35B-A3B-MTP-heretic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=GPU-00d31b08-e71c-a0ad-7f0f-62ee482cda42,GPU-d2c7640f-db52-04c5-6d03-6635359d91a9 \
 /root/llama.cpp/build/bin/llama-server \
-  -m /models/Models/llmfan46/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-GGUF/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-Q6_K.g>
-  --mmproj /models/Models/llmfan46/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-GGUF/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved->
+  -m /models/Models/llmfan46/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-GGUF/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-Q6_K.gguf \
+  --mmproj /models/Models/llmfan46/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-GGUF/Qwen3.6-35B-A3B-uncensored-heretic-Native-MTP-Preserved-mmproj-BF16.gguf \
   --n-gpu-layers 999 \
   --tensor-split 5,3 \
   --flash-attn on \
