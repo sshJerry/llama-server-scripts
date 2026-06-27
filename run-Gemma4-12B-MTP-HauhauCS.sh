980838fc4a75e804910fc2a5c86f436cb8c79247
Jerry <defineantoine@gmail.com>
2026-06-27 07:52:58 -0400

    patched commit - wrong script

--- run-Gemma4-12B-MTP-HauhauCS.sh
+++ run-Gemma4-12B-MTP-HauhauCS.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=GPU-00d31b08-e71c-a0ad-7f0f-62ee482cda42,GPU-d2c7640f-db52-04c5-6d03-6635359d91a9 \
 /root/llama.cpp/build/bin/llama-server \
-  -m /models/Models/HauhauCS/Gemma4-26B-A4B-QAT-Uncensored-HauhauCS-Balanced-MTP/Gemma4-26B-A4B-QAT-Uncensored-HauhauCS-Balanced-Q4_K_M.gguf \
-  -md /models/Models/HauhauCS/Gemma4-26B-A4B-QAT-Uncensored-HauhauCS-Balanced-MTP/mtp-gemma-4-26B-A4B-it.gguf \
-  --mmproj /models/Models/HauhauCS/Gemma4-26B-A4B-QAT-Uncensored-HauhauCS-Balanced-MTP/mmproj-Gemma4-26B-A4B-QAT-Uncensored-HauhauCS-Balanced-BF16.gguf \
+  -m /models/Models/HauhauCS/Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced/Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced-Q4_K_M.gguf \
+  -md /models/Models/HauhauCS/Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced/mtp-gemma-4-12B-it.gguf \
+  --mmproj /models/Models/HauhauCS/Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced/mmproj-Gemma4-12B-QAT-Uncensored-HauhauCS-Balanced-BF16.gguf \
   --n-gpu-layers 999 \
-  --tensor-split 7,2 \
   --flash-attn on \
   -c 262144 \
   -b 4096 \
